521befbe81554edc9853d3e6e72ff37c0754d8b3
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-11 23:01:31 +0100

    Configure webpack dev with webpack-dev-server

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    entry: {
+        index: './src/client/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+};
