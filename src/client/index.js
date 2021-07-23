cf5007083dd083db189be9cd0bbd359f05ba8eb9
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-23 04:51:55 +0100

    Fix: Add a debounce function to the user input
    By choice I query for 5 row per user input, the api call is fired with
    every input change wich resulted in 5 results for every single input.
    The solution of emptyListContainer() worked at a certain degree and
    failed with rapid inputs.
    This fix brings a debouncing function to mitigate this bug.

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,18 @@ import './styles/base.scss';
 import './styles/form.scss';
 import autocomplete from './js/autocomplete';
 
-// Input typing event.
-document.querySelector('#country-input').addEventListener('input', (event) => {
-    autocomplete(event.target);
-});
+// Debouce function
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+const debouncedAutocomplete = debounce((event) => autocomplete(event.target), 300);
+
+document.querySelector('#country-input').addEventListener('input', (e) => debouncedAutocomplete(e));
 
 // // Updates value with selected country from the suggestion list.
 // document.querySelector('.autocomplete-list-item').addEventListener()
