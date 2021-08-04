6ed8da4a6c30c6480ea0759da30b2036569c0cf1
Anes-Marzooki <marzoukianas@gmail.com>
2021-08-04 17:35:28 +0100

    Configure Asset Module to work with images

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,38 +6,38 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-        proxy: {
-            '/': {
-                target: 'http://localhost:3000/',
-            },
-        },
-        contentBase: path.resolve(__dirname, 'dist'),
-        hot: true,
-        overlay: {
-            warnings: true,
-            errors: true,
-        },
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000/',
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                /* Shortcut to: rules.use: [ { loader: 'style-loader, ...}]
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        /* Shortcut to: rules.use: [ { loader: 'style-loader, ...}]
                 *@see: https://webpack.js.org/configuration/module/#ruleuse
                 * */
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            dry: true,
-            verbose: true,
-            cleanStaleWebpackAssets: false,
-            protectWebpackAssets: false,
-        }),
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: false,
+      protectWebpackAssets: false,
+    }),
+  ],
 });
