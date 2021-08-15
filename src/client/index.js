73777d89f45445c326c6432871015a97c7ea0302
Anes-Marzooki <marzoukianas@gmail.com>
2021-08-15 16:07:40 +0100

    Add function to require all the files inside the directory styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,19 +1,40 @@
-import './styles/base.scss';
-import './styles/form.scss';
-// import test from './img/test.png';
-// import seaFront from './img/sea-front.png'
-// require './img'/;
 import countryInputHandler from './js/countryInputHandler';
 
-let req = require.context('./img/', false, /.*\.png/);
-req.keys().forEach((key) => {
-  req(key);
+/**
+*The following import functions are a solution to require all the files in the
+* img/ and styles/ directories using the context module API
+*@see: https://webpack.js.org/guides/dependency-management/#context-module-api
+*/
+const importAllStyles = (request) => {
+  request.keys().forEach((key) => request(key));
+};
+importAllStyles(require.context('./styles/', false, /\.scss$/));
+
+const imagesImport = require.context('./img/', false, /.*\.(png|jpg)/);
+imagesImport.keys().forEach((key) => {
+  imagesImport(key);
 });
 
 // // Updates value with selected country from the suggestion list.
 document.querySelector('#country-input').addEventListener('input', (e) => countryInputHandler(e));
 
 // document.querySelector('img').src = test;
+
+/*
+Grap data from the user.
+Show the user his input and ask him to confirm it.
+Send data to the server and add it to the list of trips.
+Communicate to the user that his trip was succesfully saved.
+*/
+document.querySelector('.form__submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  const countryData = document.querySelector('#country-input').value;
+  const date = document.querySelector('#date').value;
+  console.log(countryData, date);
+});
+
+// This adds image background to hero
+// document.querySelector('.hero').style.background = 'url(./img/hero.jpg)';
 
 if (module.hot) {
   module.hot.accept();
