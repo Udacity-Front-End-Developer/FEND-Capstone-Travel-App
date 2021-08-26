3eac2432801356bdebcb182e4f224fcfcbe54f9d
Anes-Marzooki <marzoukianas@gmail.com>
2021-08-27 00:11:31 +0100

    Add methods to storage to work with objects

---
+++ src/client/js/storage.js
@@ -0,0 +1,16 @@
+// Add setObjectItem method to the storage that adds object types.
+Storage.prototype.setObjectItem = function setObjectItem(key, value) {
+    const obj = JSON.stringify(value);
+    this.setItem(key, obj);
+};
+
+// Add getObjectItem method to the storage that gets object types.
+Storage.prototype.getObjectItem = function getObjectItem(key) {
+    return JSON.parse(this.getItem(key));
+};
+
+const addTrip = (trip) => {
+    const arrayOfTrips = localStorage.getObjectItem('trips');
+    arrayOfTrips.push(trip);
+    localStorage.setObjectItem('trips', arrayOfTrips);
+};
